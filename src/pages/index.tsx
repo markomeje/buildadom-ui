d8ac5f18efe33214d664bde25cc3834daffb095c
ifenna-pascal <ifennamonanu70@gmail.com>
2023-03-23 16:29:02 +0100

    initial commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head'
 
-
 export default function Home() {
   return (
     <>
@@ -10,7 +9,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-       <h1 className='text-green-400' >Wecome To Next Init</h1>
+      <h1 className="text-green-400">Wecome To Next Init</h1>
     </>
   )
 }
