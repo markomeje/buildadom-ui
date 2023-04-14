efb7d2c2a7cc55a20514eb780e479bf0b7cf9e78
ifenna-pascal <ifennamonanu70@gmail.com>
2023-04-14 12:46:29 +0100

    fix: resolved error on deployment

--- src/redux/services/general.service.ts
+++ src/redux/services/general.service.ts
@@ -2,17 +2,15 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
-import { RootState } from '../store'
 
 // auth service build
 
 export const generalApi = createApi({
-  reducerPath: 'storeApi',
+  reducerPath: 'generalApi',
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.buildadom.net/api/v1',
     headers: { accept: 'application/json' },
   }),
-  tagTypes: ['Product', 'Store'],
   endpoints: (builder) => ({
     // QUERIES
     allProducts: builder.query<any, void>({
